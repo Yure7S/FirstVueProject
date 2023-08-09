68df124f34e54465f1c87a88891122353ab93e85
Yure7S <yuresilvaprof@gmail.com>
2023-08-09 09:55:36 -0300

    Atualizando

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -16,7 +16,7 @@ let login = async () => {
 
 <template>
     <section class="w-11/12 h-md mx-auto mt-28 max-w-screen-2 rounded-lg overflow-hidden flex">
-        <form @submit.prevent="login" class="bg-base-contrast rounded-lg w-full h-full md:w-1/2 mx-auto max-w-sm">
+        <form @submit.prevent="login" class="bg-base-contrast mx-auto rounded-lg max-w-sm w-full h-full md:w-1/2 md:max-w-none">
             <div class="w-full h-full flex flex-col justify-center align-middle">
                 <h2 class=" text-main font-bold text-4xl text-center mb-11">Login</h2>
                 <input v-model="loginData.email" class="w-2/3 my-3 mx-auto p-2 text-colortx-dark rounded-sm bg-secondary" placeholder="E-mail" required type="text">
