a7710299c7565501d7675f13b3180aa7c7cf556b
Yure7S <yuresilvaprof@gmail.com>
2023-08-09 15:12:26 -0300

    Preparando a dinamicidade do componente course card

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -2,18 +2,24 @@
 import { RouterLink } from "vue-router";
 import Link from "./common/link.vue";
 import Button from "../components/Common/Button.vue"
+
+defineProps({
+    rote: String,
+
+})
+
 </script>
 
 <template>
     <div class="border-base-contrast-02 border border-t-8 rounded p-5 w-full max-w-screen-1 bg-base-contrast overflow-hidden">
-        <h3 name="title">Engenharia civil</h3>
-        <h4 class=" text-colortx-soft" name="sub-title">Bacherelado - 5 anos</h4>
+        <h3 name="title"><slot name="title"></slot></h3>
+        <h4 class=" text-colortx-soft" name="sub-title"><slot name="tipe-course"></slot></h4>
         <div class=" bg-base-contrast-02 px-3 py-4 p-b my-4 rounded-sm">
-            <p>a partir de</p>
-            <p><span class="font-bold text-xl">R$ 1530,00</span> por mês</p>
+            <p>as from</p>
+            <p><span class="font-bold text-xl">R$ <slot name="price"></slot></span> a month</p>
         </div>
-        <Link class="text-colortx-soft text-sm" to="/" content="SAIBA MAIS"></Link>
+        <Link class="text-colortx-soft text-sm" to="/" content="LEARN MORE"></Link>
         <hr class=" mt-8 text-base-contrast-02">
-        <Button class=" text-sm text-main border mt-5 float-right" content="INSCREVA-SE"></Button>
+        <Button class=" text-sm text-main border mt-5 float-right" content="SIGN UP"></Button>
     </div>
 </template>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,16 +1,33 @@
 <script setup>
 import CourseCard from '../components/CourseCard.vue';
+
+let itens = [
+  {
+    nome: "Culinária",
+    tempo: "Bacharelado - 5 years",
+    price: "1.000"
+  },
+  {
+    nome: "Engenharia",
+    tempo: "Bacharelado - 4 years",
+    price: "1.350"
+  }
+]
+
 </script>
 
 <template>
   <section class="flex max-w-screen-1 gap-10 mt-20 flex-wrap mx-auto md:max-w-screen-3 lg:max-w-screen-4">
-    <CourseCard/>
-    <CourseCard/>
-    <CourseCard/>
-    <CourseCard/>
-    <CourseCard/>
-    <CourseCard/>
-    <CourseCard/>
-    <CourseCard/>
+    <CourseCard v-for="element in itens">
+      <template #title>
+        {{ element.nome }}
+      </template>
+      <template #tipe-course>
+        {{ element.tempo }}
+      </template>
+      <template #price>
+        {{ element.price }}
+      </template>
+    </CourseCard>
   </section>
 </template>
