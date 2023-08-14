92fbdface3161aec9b33eee88bb269dfb9758459
Yure7S <yuresilvaprof@gmail.com>
2023-08-14 16:08:20 -0300

    Resolvendo bug ao usar ref, mostrando cursos do banco

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -5,7 +5,7 @@ import app from '../config/axios.config';
 import { onMounted, ref } from 'vue';
 
 const auth = userAuthStore()
-let data = ref()
+let corses = ref([])
 
 onMounted(async () => {
   let response = await app.get("/courses", {
@@ -13,15 +13,14 @@ onMounted(async () => {
       'Authorization': `Bearer ${auth.token}`
     }
   })
-  data = response.data
-  console.log(data)
+  corses.value = response.data
 })
 </script>
 
 <template>
   <h1>Testando</h1>
   <section class="flex max-w-screen-1 gap-10 mt-20 flex-wrap mx-auto md:max-w-screen-3 lg:max-w-screen-4">
-        <CourseCard v-for="item in data" :key="item.code" rote="/">
+        <CourseCard v-for="item in corses" :key="item.code" rote="/">
           <template #title>
             {{ item.fullName }}
           </template>
